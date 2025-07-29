15ce87b5fa0a6eabe67cccd223555a9490578e09
averyhsu <averyhsuhsu@gmail.com>
2025-07-29 13:03:11 +0800

    simulation: basic movements

--- simulation_ws/src/robot_control/simple_controller.cpp
+++ simulation_ws/src/robot_control/simple_controller.cpp
@@ -19,11 +19,11 @@ int main(int argc, char **argv)
     ros::Publisher left_knee_pub = nh.advertise<std_msgs::Float64>("/wheeled_biped/left_knee_position_controller/command", 10);
     ros::Publisher right_knee_pub = nh.advertise<std_msgs::Float64>("/wheeled_biped/right_knee_position_controller/command", 10);
 
-    ros::Publisher left_wheel_pub = nh.advertise<std_msgs::Float64>("/wheeled_biped/left_wheel_velocity_controller/command", 10);
-    ros::Publisher right_wheel_pub = nh.advertise<std_msgs::Float64>("/wheeled_biped/right_wheel_velocity_controller/command", 10);
+    ros::Publisher left_wheel_pub = nh.advertise<std_msgs::Float64>("/wheeled_biped/left_wheel_effort_controller/command", 10);
+    ros::Publisher right_wheel_pub = nh.advertise<std_msgs::Float64>("/wheeled_biped/right_wheel_effort_controller/command", 10);
 
     // Wait a moment for everything to initialize
-    ros::Duration(1.0).sleep();
+    ros::Duration(3.0).sleep();
 
     // Set the control loop rate (e.g., 100 Hz)
     ros::Rate loop_rate(100);
@@ -41,26 +41,29 @@ int main(int argc, char **argv)
         // double frequency = 0.5; // Hz
         // double desired_pos = amplitude * sin(2 * M_PI * frequency * elapsed_time);
 
-        std_msgs::Float64 hip_cmd;
-        hip_cmd.data = 0.0;
-        left_hip_pub.publish(hip_cmd);
-        right_hip_pub.publish(hip_cmd);
+        // std_msgs::Float64 hip_cmd;
+        // hip_cmd.data = deg_to_rad(0);
+        // left_hip_pub.publish(hip_cmd);
+        // right_hip_pub.publish(hip_cmd);
 
-        // Knee position control
-        std_msgs::Float64 knee_cmd;
-        knee_cmd.data = deg_to_rad(90);
-        left_knee_pub.publish(knee_cmd);
-        right_knee_pub.publish(knee_cmd);
+        // // Knee position control
+        // std_msgs::Float64 knee_cmd;
+        // knee_cmd.data = deg_to_rad(0);
+        // left_knee_pub.publish(knee_cmd);
+        // right_knee_pub.publish(knee_cmd);
+
+
 
         // --- Wheel Velocity Control ---
         // Command the wheels to move forward at a constant velocity
         std_msgs::Float64 wheel_cmd;
-        wheel_cmd.data = 0.0; // rad/s
+        wheel_cmd.data = 1; // rad/s
         left_wheel_pub.publish(wheel_cmd);
         right_wheel_pub.publish(wheel_cmd);
 
         ros::spinOnce();
         loop_rate.sleep();
-    }
+   }
     return 0;
 }
+
